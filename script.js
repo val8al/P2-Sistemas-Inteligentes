6a44017e4a39f4a209b40a8ac25fd278d2348e7a
valentin <A01371084@itesm.mx>
2020-04-14 14:35:43 -0500

    Pawn only game now detects victory correctly

--- script.js
+++ script.js
@@ -1,69 +1,52 @@
-const BOARD_FEN_STRING='r7/pp5k/7p/3P2p1/8/PP5P/1R5K/8 w - - 0 36'
+
+
+/*
+8/pp5k/p6p/3P1p1/8/3PK1/2P4P/8 w - - 0 36
+
+r7/pp5k/7p/3P2p1/8/PP5P/1R5K/8 w - - 0 36
+
+rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w - - 0 1
+
+*/
+const BOARD_FEN_STRING='4k3/pppppppp/8/8/8/8/PPPPPPPP/4K3 w - - 0 1'
+var victory_flag = false;
 var board,
     game = new Chess(BOARD_FEN_STRING);
 
 /*The "AI" part starts here */
 
-var minimaxRoot =function(depth, game, isMaximisingPlayer) {
+var uninformedRoot = function(game){
 
-    var newGameMoves = game.ugly_moves();
-    var bestMove = -9999;
-    var bestMoveFound;
-
-    for(var i = 0; i < newGameMoves.length; i++) {
-        var newGameMove = newGameMoves[i]
-        game.ugly_move(newGameMove);
-        var value = minimax(depth - 1, game, -10000, 10000, !isMaximisingPlayer);
+    var possibleMoves = game.ugly_moves();
+    var bestMoveScore = -9999;
+    var bestMoveFound = possibleMoves[0]
+    for(var i = 0; i < possibleMoves.length; i++){//for every possible move
+        game.ugly_move(possibleMoves[i]);
+        var boardEval = -evaluateBoard(game.board());//do it, check the outcome 
+        //console.log("bval ="+ boardEval+"|bmf = "+bestMoveScore)
+        if(boardEval >= bestMoveScore){
+            bestMoveScore = boardEval
+            bestMoveFound = possibleMoves[i]
+            //compare it to the other outcomes and declare best if so
+        }
         game.undo();
-        if(value >= bestMove) {
-            bestMove = value;
-            bestMoveFound = newGameMove;
-        }
     }
-    return bestMoveFound;
-};
-
-var minimax = function (depth, game, alpha, beta, isMaximisingPlayer) {
-    positionCount++;
-    if (depth === 0) {
-        return -evaluateBoard(game.board());
-    }
-
-    var newGameMoves = game.ugly_moves();
-
-    if (isMaximisingPlayer) {
-        var bestMove = -9999;
-        for (var i = 0; i < newGameMoves.length; i++) {
-            game.ugly_move(newGameMoves[i]);
-            bestMove = Math.max(bestMove, minimax(depth - 1, game, alpha, beta, !isMaximisingPlayer));
-            game.undo();
-            alpha = Math.max(alpha, bestMove);
-            if (beta <= alpha) {
-                return bestMove;
-            }
-        }
-        return bestMove;
-    } else {
-        var bestMove = 9999;
-        for (var i = 0; i < newGameMoves.length; i++) {
-            game.ugly_move(newGameMoves[i]);
-            bestMove = Math.min(bestMove, minimax(depth - 1, game, alpha, beta, !isMaximisingPlayer));
-            game.undo();
-            beta = Math.min(beta, bestMove);
-            if (beta <= alpha) {
-                return bestMove;
-            }
-        }
-        return bestMove;
-    }
-};
+    return bestMoveFound
+}
 
 var evaluateBoard = function (board) {
+   
     var totalEvaluation = 0;
     for (var i = 0; i < 8; i++) {
         for (var j = 0; j < 8; j++) {
-            
-            totalEvaluation = totalEvaluation + getPieceValue(board[i][j], i ,j);
+            iterationPieceValue = getPieceValue(board[i][j], i ,j)
+
+            if(iterationPieceValue >= 5000 
+                ||iterationPieceValue <=-5000 ){
+                    victory_flag =true;
+            }
+
+            totalEvaluation = totalEvaluation + iterationPieceValue;
         }
     }
     return totalEvaluation;
@@ -72,6 +55,8 @@ var evaluateBoard = function (board) {
 var reverseArray = function(array) {
     return array.slice().reverse();
 };
+
+
 
 var pawnEvalWhite =
     [
@@ -87,30 +72,6 @@ var pawnEvalWhite =
 
 var pawnEvalBlack = reverseArray(pawnEvalWhite);
 
-var knightEval =
-    [
-        [-5.0, -4.0, -3.0, -3.0, -3.0, -3.0, -4.0, -5.0],
-        [-4.0, -2.0,  0.0,  0.0,  0.0,  0.0, -2.0, -4.0],
-        [-3.0,  0.0,  1.0,  1.5,  1.5,  1.0,  0.0, -3.0],
-        [-3.0,  0.5,  1.5,  2.0,  2.0,  1.5,  0.5, -3.0],
-        [-3.0,  0.0,  1.5,  2.0,  2.0,  1.5,  0.0, -3.0],
-        [-3.0,  0.5,  1.0,  1.5,  1.5,  1.0,  0.5, -3.0],
-        [-4.0, -2.0,  0.0,  0.5,  0.5,  0.0, -2.0, -4.0],
-        [-5.0, -4.0, -3.0, -3.0, -3.0, -3.0, -4.0, -5.0]
-    ];
-
-var bishopEvalWhite = [
-    [ -2.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -2.0],
-    [ -1.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -1.0],
-    [ -1.0,  0.0,  0.5,  1.0,  1.0,  0.5,  0.0, -1.0],
-    [ -1.0,  0.5,  0.5,  1.0,  1.0,  0.5,  0.5, -1.0],
-    [ -1.0,  0.0,  1.0,  1.0,  1.0,  1.0,  0.0, -1.0],
-    [ -1.0,  1.0,  1.0,  1.0,  1.0,  1.0,  1.0, -1.0],
-    [ -1.0,  0.5,  0.0,  0.0,  0.0,  0.0,  0.5, -1.0],
-    [ -2.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -2.0]
-];
-
-var bishopEvalBlack = reverseArray(bishopEvalWhite);
 
 var rookEvalWhite = [
     [  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0],
@@ -125,16 +86,7 @@ var rookEvalWhite = [
 
 var rookEvalBlack = reverseArray(rookEvalWhite);
 
-var evalQueen = [
-    [ -2.0, -1.0, -1.0, -0.5, -0.5, -1.0, -1.0, -2.0],
-    [ -1.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -1.0],
-    [ -1.0,  0.0,  0.5,  0.5,  0.5,  0.5,  0.0, -1.0],
-    [ -0.5,  0.0,  0.5,  0.5,  0.5,  0.5,  0.0, -0.5],
-    [  0.0,  0.0,  0.5,  0.5,  0.5,  0.5,  0.0, -0.5],
-    [ -1.0,  0.5,  0.5,  0.5,  0.5,  0.5,  0.0, -1.0],
-    [ -1.0,  0.0,  0.5,  0.0,  0.0,  0.0,  0.0, -1.0],
-    [ -2.0, -1.0, -1.0, -0.5, -0.5, -1.0, -1.0, -2.0]
-];
+
 
 var kingEvalWhite = [
 
@@ -162,16 +114,12 @@ var getPieceValue = function (piece, x, y) {
             return 10 + ( isWhite ? pawnEvalWhite[y][x] : pawnEvalBlack[y][x] );
         } else if (piece.type === 'r') {
             return 50 + ( isWhite ? rookEvalWhite[y][x] : rookEvalBlack[y][x] );
-        } else if (piece.type === 'n') {
-            return 30 + knightEval[y][x];
-        } else if (piece.type === 'b') {
-            return 30 + ( isWhite ? bishopEvalWhite[y][x] : bishopEvalBlack[y][x] );
-        } else if (piece.type === 'q') {
-            return 90 + evalQueen[y][x];
         } else if (piece.type === 'k') {
             return 900 + ( isWhite ? kingEvalWhite[y][x] : kingEvalBlack[y][x] );
-        }
-        throw "Unknown piece type: " + piece.type;
+        } else{
+            return 5000;
+        } 
+       // throw "Unknown piece type: " + piece.type;
     };
 
     var absoluteValue = getAbsoluteValue(piece, piece.color === 'w', x ,y);
@@ -179,7 +127,11 @@ var getPieceValue = function (piece, x, y) {
 };
 
 
-/* board visualization and games state handling */
+/* board visualization and games state handling.
+
+
+the following was mostly written by Lauri Hartikka 
+in freecodecamp.org/news/simple-chess-ai-step-by-step-1d55a9266977/*/
 
 var onDragStart = function (source, piece, position, orientation) {
     if (game.in_checkmate() === true || game.in_draw() === true ||
@@ -193,7 +145,7 @@ var makeBestMove = function () {
     game.ugly_move(bestMove);
     board.position(game.fen());
     renderMoveHistory(game.history());
-    if (game.game_over()) {
+    if (game.game_over()||victory_flag) {
         alert('Game over');
     }
 };
@@ -201,7 +153,8 @@ var makeBestMove = function () {
 
 var positionCount;
 var getBestMove = function (game) {
-    if (game.game_over()) {
+  
+    if (game.game_over()||victory_flag) {
         alert('Game over');
     }
 
@@ -209,7 +162,7 @@ var getBestMove = function (game) {
     var depth = parseInt($('#search-depth').find(':selected').text());
 
     var d = new Date().getTime();
-    var bestMove = minimaxRoot(depth, game, true);
+    var bestMove = uninformedRoot(game);//call for making move to AI
     var d2 = new Date().getTime();
     var moveTime = (d2 - d);
     var positionsPerS = ( positionCount * 1000 / moveTime);
